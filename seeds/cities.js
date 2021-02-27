a09c274a2fc4c866029159e7f54d92a8213d8306
Varsha Panchal <varsha3051@gmail.com>
2021-02-26 19:04:46 -0500

    Connect Routes and DB
    Express(Back-end Web app framework ),
    Routes,
    connect views and models with routes ,
    Mongoose,
     -seeds

---
+++ seeds/cities.js
@@ -0,0 +1,40 @@
+module.exports = [
+    {
+        city: "Sarnia",
+        growth_from_2000_to_2013: "4.8%",
+        latitude: 42.9994,
+        longitude: -82.3089,
+        population: "71594",
+        rank: "1",
+        state: "Ontario"
+    },
+    {
+        city: "Montreal",
+        growth_from_2000_to_2013: "4.8%",
+        latitude: 45.5089,
+        longitude: -73.5617,
+        population: "3519595",
+        rank: "2",
+        state: "Quebec"
+    },
+    
+    {
+        city: "Duncan",
+        growth_from_2000_to_2013: "4.8%",
+        latitude: 48.7787,
+        longitude: -123.7079,
+        population: "44451",
+        rank: "3",
+        state: "British Columbia"
+    },
+    
+    {
+        city: "Winkler",
+        growth_from_2000_to_2013: "4.8%",
+        latitude: 49.1817,
+        longitude: -97.9397,
+        population: "12660",
+        rank: "4",
+        state: "Manitoba"
+    }
+]
---
+++ seeds/seedHelpers.js
@@ -0,0 +1,25 @@
+const descriptors =[
+    'RedWood',
+    'Maple',
+    'Ocean',
+    'Sea',
+    'Sky',
+    'Forest',
+    'Dusty',
+    'Dimond',
+    'Petrified',
+    'Silent'
+]
+const places = [
+    'Pond',
+    'Village',
+    'Ground Camp',
+    'Ghost Town',
+    'Camp',
+    'River',
+    'Bay',
+    'Sand',
+    'Hunting Camp',
+    'Spring'
+
+]
