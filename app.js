fd867f0f5a63d564cb5747f572b0c57a58ebf3a1
Varsha Panchal <varsha3051@gmail.com>
2021-03-01 15:10:51 -0500

    Create new Campground

--- app.js
+++ app.js
@@ -17,8 +17,11 @@ db.once("open", () => {
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended: true}))  //create new url
 
 app.get('/', (req,res) => {
     res.render('home')
@@ -27,6 +30,15 @@ app.get('/', (req,res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
+})
+app.get('/campgrounds/new', (req,res) => {    
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async(req, res) => {
