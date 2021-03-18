44e46745889e763727260eadfb2315ddf634376a
Varsha Panchal <varsha3051@gmail.com>
2021-03-18 16:54:39 -0400

    Add Athentication
    Used Passport

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const flash = require('connect-flash');
 //const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users'); 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -30,17 +34,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-app.use(express.urlencoded({extended: true}))  //create new url
+app.use(express.urlencoded({extended: true}))           //create new url
 app.use(methodOverride('_method'));
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!!',
     resave: false,
     saveUninitialized: true,
-    cookie: {                           //send back cookies
-        httpOnly: true,             //secure httpOnly
+    cookie: {                               //send back cookies
+        httpOnly: true,                   //secure httpOnly
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -49,14 +51,25 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());                               //store user in session
+passport.deserializeUser(User.deserializeUser());                         //how do you get a user out of session
+
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
+
 
 app.get('/', (req, res) => {
     res.render('home')
